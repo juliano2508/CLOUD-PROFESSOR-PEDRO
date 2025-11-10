934b9aa5d857dff0c16753fd3654dd4c3e7b416d
Juliano Cesar <juliano.cesar.m2@gmail.com>
2025-11-10 11:06:18 -0300

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask
 import json
+import psutil
+import os
+import platform
+
+
+print(psutil.cpu_percent(percpu=True))
+
+print(psutil.virtual_memory().used / 1024 ** 2)
+
+print(os.getpid())
+
+print(platform.platform())
+
+
 
 APP = Flask(__name__)
 
